bebfc817fc25e44a7c768d6514e1d53c9f7d0399
61911 <psp.11.jb@gmail.com>
2023-06-05 22:00:28 -0400

    Added nav, webpack, integrate semantic UI

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,12 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import "@hotwired/turbo-rails";
+import "controllers";
+import "jquery";
+import "popper.js";
+import "@doabit/semantic-ui-sass";
+
+import $ from "jquery";
+
+$(document).on("turbolinks:load", function () {
+  $(".ui.dropdown").dropdown();
+});
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,9 @@
 # Pin npm packages by running ./bin/importmap
 
+# Configure importmap for JavaScript modules
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "@doabit/semantic-ui-sass", to: "semantic-ui.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
